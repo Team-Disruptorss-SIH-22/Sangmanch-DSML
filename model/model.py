f123d9d1ec08d9c8190748137b63679ef421f715
Ashish Papanai <ashishpapanai00@gmail.com>
2022-08-26 02:44:53 +0530

    Added: model defs

--- model/model.py
+++ model/model.py
@@ -1,7 +1,7 @@
 import pyod.models.xgbod as XGBOD
 
 class Model:
-    def __init__(self, X, y=None):
+    def __init__(self, X, y=None, model=None):
         if y is None:
             self.model = self.train(X)
         else:
@@ -9,11 +9,24 @@ class Model:
         self.prediction = self.predict(X) # Predict the model
 
 
-    def train(self, X, y):
-        from pyod.models.ecod import ECOD
-        clf = XGBOD() # Using default settings
-        clf.fit(X, y) # Train the model
-        return clf
+    def train(self, X, y=None, model=None):
+
+        if model is 'XGBOD':
+            model = XGBOD.XGBOD(max_depth=3, learning_rate=0.1, n_estimators=100, verbose=True)
+            if y == None:
+                model.fit(X)
+            else:
+                model.fit(X, y)
+            return model
+
+        elif model == 'LogReg':
+            from sklearn.linear_model import LogisticRegression
+            model = LogisticRegression()
+            if y == None:
+                raise Exception('Logistic Regression requires y')
+            else:
+                model.fit(X, y)
+            return model
     
     def predict(self, X):
         return self.model.predict(X) # Predict the target values

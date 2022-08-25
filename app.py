e490dc4f87fab82cf83dd8fee61c919960e0bbab
Ashish Papanai <ashishpapanai00@gmail.com>
2022-08-25 23:32:48 +0530

    Modified: ports

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from info import finance, user
-
+import os
 app = Flask(__name__)
 
 
@@ -35,5 +35,6 @@ def monthly():
 	return usr.monthlyData()
 
 
-if __name__ == '__main__':
-	app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
